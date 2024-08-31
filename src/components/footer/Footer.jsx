0bc537cbcf9e171168a25df9dda6dadd548e2ca5
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-08-31 08:17:50 +0100

    fix: fix footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="footer">
       <div className="footer__top">
         <div className="footer__item">
-          <h2 className="footer__h1">Categories</h2>
+          <h2 className="footer__h2">Categories</h2>
           <span className="footer__span">Women</span>
           <span className="footer__span">Men</span>
           <span className="footer__span">Shoes</span>
@@ -11,7 +11,7 @@ const Footer = () => {
           <span className="footer__span">New Arrivals</span>
         </div>
         <div className="footer__item">
-          <h2 className="footer__h1">Links</h2>
+          <h2 className="footer__h2">Links</h2>
           <span className="footer__span">FAQ</span>
           <span className="footer__span">Pages</span>
           <span className="footer__span">Stores</span>
@@ -19,18 +19,16 @@ const Footer = () => {
           <span className="footer__span">Cookies</span>
         </div>
         <div className="footer__item">
-          <h2 className="footer__h1">About</h2>
+          <h2 className="footer__h2">About</h2>
           <p className="footer__p">
             Welcome to Superstore, the UK's premier destination for stylish and
             affordable fashion. Located in the heart of the UK, Superstore is
             your go-to shop for clothing that combines quality, comfort, and the
-            latest trends. Whether you're refreshing your wardrobe, shopping for
-            a special occasion, or looking for everyday essentials, Superstore
-            has something for everyone.
+            latest trends.
           </p>
         </div>
         <div className="footer__item">
-          <h2 className="footer__h1">Contact</h2>
+          <h2 className="footer__h2">Contact</h2>
           <p className="footer__p">
             Customer Service: Phone: +44 (0)20 1234 5678 Email:
             support@superstore.co.uk Hours: Monday to Saturday, 9 AM - 6 PM
